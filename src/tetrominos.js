d67dd3e5c19a87a9eb6c7134a9622a45f8d2d33d
nhat-anh <japananh@gmail.com>
2019-08-22 14:35:11 +0700

    Add styles for all components

--- src/tetrominos.js
+++ src/tetrominos.js
@@ -1,5 +1,5 @@
 export const TETROMINOS = {
-  0: { shape: [[0]], color: "0,0,0" },
+  0: { shape: [[0]], color: '0,0,0' },
   I: {
     shape: [
       [0, 'I', 0, 0],
@@ -55,7 +55,7 @@ export const TETROMINOS = {
       [0, 0, 0]
     ],
     color: '227, 78, 78'
-  },
+  }
 };
 
 export const randomTetromino = () => {
